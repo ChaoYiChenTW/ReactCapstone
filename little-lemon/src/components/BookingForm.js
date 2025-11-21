16c85a798d250dd2134fd718509ad58d5ff99258
Chao-Yi Chen <a951159a@gmail.com>
2025-11-21 09:43:18 +0000

    Styling Booking page

--- little-lemon/src/components/BookingForm.js
+++ little-lemon/src/components/BookingForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import '../styles/style.css'
 
 function generateTimes(start = 17, end = 21, interval = 60) {
   const times = [];
@@ -33,51 +34,56 @@ export default function BookingForm() {
     };
 
     return (
-        <form
-            style={{ display: "grid", maxWidth: "200px", gap: "20px" }}
-            onSubmit={handleSubmit}
-        >
-            <label htmlFor="res-date">Choose date</label>
-            <input
-                type="date"
-                id="res-date"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-            />
+        <form className="booking-form" onSubmit={handleSubmit}>
+            <div className="booking-form-field">
+                <label htmlFor="res-date">Choose date</label>
+                <input
+                    type="date"
+                    id="res-date"
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                />
+            </div>
 
-            <label htmlFor="res-time">Choose time</label>
-            <select
-            id="res-time"
-                value={selectedTime}
-                onChange={(e) => setSelectedTime(e.target.value)}
-                >
-                {availableTimes.map((t) => (
-                    <option key={t} value={t}>
-                    {t}
-                    </option>
-                ))}
-            </select>
+            <div className="booking-form-field">
+                <label htmlFor="res-time">Choose time</label>
+                    <select
+                    id="res-time"
+                        value={selectedTime}
+                        onChange={(e) => setSelectedTime(e.target.value)}
+                        >
+                        {availableTimes.map((t) => (
+                            <option key={t} value={t}>
+                            {t}
+                            </option>
+                        ))}
+                    </select>
+            </div>
 
-            <label htmlFor="guests">Number of guests</label>
-            <input
-                type="number"
-                id="guests"
+            <div className="booking-form-field">
+                <label htmlFor="guests">Number of guests</label>
+                <input
+                    type="number"
+                    id="guests"
                 min="1"
                 max="10"
                 value={guests}
                 onChange={(e) => setGuests(e.target.value)}
-            />
+                />
+            </div>
 
-            <label htmlFor="occasion">Occasion</label>
-            <select
-                id="occasion"
-                value={occasion}
-                onChange={(e) => setOccasion(e.target.value)}
-            >
-                <option value="Birthday">Birthday</option>
-                <option value="Anniversary">Anniversary</option>
-                <option value="None">None</option>
-            </select>
+            <div className="booking-form-field">
+                <label htmlFor="occasion">Occasion</label>
+                <select
+                    id="occasion"
+                    value={occasion}
+                    onChange={(e) => setOccasion(e.target.value)}
+                >
+                    <option value="Birthday">Birthday</option>
+                    <option value="Anniversary">Anniversary</option>
+                    <option value="None">None</option>
+                </select>
+            </div>
 
             <input type="submit" value="Make Your reservation" />
         </form>
